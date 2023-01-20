bfd4cf7303328f87e36c49ce52078a5a55c2e94e
sshrik <chj878194@naver.com>
2023-01-21 02:18:01 +0900

    feat: add primary color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      primary: '#53B860',
+      'primary-hover': '#4FA55A',
+    },
   },
   plugins: [],
 };
