120892f2fa8d368e898983b65bfb5674976696ff
sshrik <chj878194@naver.com>
2023-01-21 00:56:51 +0900

    feat: add storybook and eslint-storybook-plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,8 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
   ],
-
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
