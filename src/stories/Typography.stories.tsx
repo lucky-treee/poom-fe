0bbc51f413330dbbd1b539cbca223d87dcde32fd
sshrik <chj878194@naver.com>
2023-01-21 16:57:48 +0900

    feat: remove unicode setting and combine Typography stories regardless of language

--- src/stories/Typography.stories.tsx
+++ src/stories/Typography.stories.tsx
@@ -7,34 +7,30 @@ export default {
   component: Typography,
 };
 
-export const EnglishTitle: ComponentStory<typeof Typography> = () => (
-  <Typography type="title">English Title Component</Typography>
+export const Title: ComponentStory<typeof Typography> = () => (
+  <div className="flex flex-col">
+    <Typography type="title">English Title Component</Typography>
+    <Typography type="title">한글 제목 컴포넌트</Typography>
+  </div>
 );
 
-export const KoreanTitle: ComponentStory<typeof Typography> = () => (
-  <Typography type="title">한글 제목 컴포넌트</Typography>
+export const SubTitle: ComponentStory<typeof Typography> = () => (
+  <div className="flex flex-col">
+    <Typography type="subtitle">English SubTitle Component</Typography>
+    <Typography type="subtitle">한글 부제목 컴포넌트</Typography>
+  </div>
 );
 
-export const EnglishSubTitle: ComponentStory<typeof Typography> = () => (
-  <Typography type="subtitle">English SubTitle Component</Typography>
+export const BodyText: ComponentStory<typeof Typography> = () => (
+  <div className="flex flex-col">
+    <Typography type="body">English Body Component</Typography>
+    <Typography type="body">한글 본문 컴포넌트</Typography>
+  </div>
 );
 
-export const KoreanSubTitle: ComponentStory<typeof Typography> = () => (
-  <Typography type="subtitle">한글 부제목 컴포넌트</Typography>
-);
-
-export const EnglishBody: ComponentStory<typeof Typography> = () => (
-  <Typography type="body">English Body Component</Typography>
-);
-
-export const KoreanBody: ComponentStory<typeof Typography> = () => (
-  <Typography type="body">한글 본문 컴포넌트</Typography>
-);
-
-export const EnglishCaption: ComponentStory<typeof Typography> = () => (
-  <Typography type="caption">English Caption Component</Typography>
-);
-
-export const KoreanCaption: ComponentStory<typeof Typography> = () => (
-  <Typography type="caption">한글 설명 컴포넌트</Typography>
+export const Caption: ComponentStory<typeof Typography> = () => (
+  <div className="flex flex-col">
+    <Typography type="caption">English Caption Component</Typography>
+    <Typography type="caption">한글 설명 컴포넌트</Typography>
+  </div>
 );
