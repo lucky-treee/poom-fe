83b4abf65c82074bbe0ea31e4eaaba08c6578bdd
sshrik <chj878194@naver.com>
2023-01-23 01:08:54 +0900

    feat: set script for dapi

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigpaths from 'vite-tsconfig-paths';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 export default defineConfig({
-  plugins: [react(), tsconfigpaths()],
+  plugins: [react(), tsconfigpaths(), VitePluginHtmlEnv()],
 });
