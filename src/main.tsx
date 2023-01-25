bf42ffd3a353c6867fe2dcf6adf67239c0b39ca0
Minsup Lee <m1nsuppp@gmail.com>
2023-01-26 00:02:32 +0900

    feat: add `msw`

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 import 'index.css';
+import { worker } from 'mocks/browser';
+
+if (process.env.VITE_NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
