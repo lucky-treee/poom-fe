76c1eb01c5817acb3b16fd8fc149e2e6f20b3430
sshrik <chj878194@naver.com>
2023-01-28 01:05:27 +0900

    feat: add svgt package to import svg

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigpaths from 'vite-tsconfig-paths';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tsconfigpaths(), VitePluginHtmlEnv()],
+  plugins: [
+    react(),
+    tsconfigpaths(),
+    VitePluginHtmlEnv(),
+    svgr({ exportAsDefault: true }),
+  ],
 });
