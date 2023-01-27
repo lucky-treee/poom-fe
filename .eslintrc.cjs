9a8bc24a7334d6bc7874e527c4c1fd6782e97e8e
sshrik <chj878194@naver.com>
2023-01-28 01:19:10 +0900

    refactor: off `react/require-default-props` rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/function-component-definition': 'off',
     'import/no-extraneous-dependencies': [
