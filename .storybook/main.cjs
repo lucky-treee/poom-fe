b286c6863e5c0bc0cb3c7ae0d6a9d6be4e56a205
sshrik <chj878194@naver.com>
2023-01-31 18:06:08 +0900

    feat: bump up version storybook 6.5 to 7.0-beta

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,6 @@
 const viteTsconfig = require('vite-tsconfig-paths');
 const tsconfigPaths = viteTsconfig.default;
 const { mergeConfig } = require('vite');
-const svgr = require('vite-plugin-svgr');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -10,16 +9,19 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
   features: {
     storyStoreV7: true,
   },
   async viteFinal(config) {
     return mergeConfig(config, {
-      plugins: [tsconfigPaths(), svgr()],
+      plugins: [tsconfigPaths()],
     });
+  },
+  docs: {
+    autodocs: true,
   },
 };
