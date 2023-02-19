2b287001bc9f8f0d8159b733490b958439d0cd72
sshrik <chj878194@naver.com>
2023-02-19 21:08:20 +0900

    feat: add `UnknownErrorPage`
    * add className for `Typography`

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -2,26 +2,43 @@ import React from 'react';
 
 type TypographyProps = {
   children: React.ReactNode;
-  type: 'title' | 'subtitle' | 'caption' | 'body';
+  className?: string;
+  type: 'title' | 'subtitle' | 'caption' | 'body' | 'code';
 };
 
 const Typography: React.FC<TypographyProps> = (props) => {
-  const { children, type } = props;
+  const { children, type, className } = props;
 
   if (type === 'title') {
-    return <h1 className="text-2xl font-bold leading-8">{children}</h1>;
+    return (
+      <h1 className={`text-2xl font-bold leading-8 ${className}`}>
+        {children}
+      </h1>
+    );
   }
   if (type === 'subtitle') {
-    return <h3 className="text-lg font-bold leading-8">{children}</h3>;
+    return (
+      <h3 className={`text-lg font-bold leading-8 ${className}`}>{children}</h3>
+    );
   }
   if (type === 'body') {
-    return <p className="text-sm font-normal leading-5">{children}</p>;
+    return (
+      <p className={`text-sm font-normal leading-5 ${className}`}>{children}</p>
+    );
   }
   if (type === 'caption') {
-    return <p className="text-xs font-normal leading-4">{children}</p>;
+    return (
+      <p className={`text-xs font-normal leading-4 ${className}`}>{children}</p>
+    );
   }
-
-  return <p className="text-sm font-normal leading-5">{children}</p>;
+  if (type === 'code') {
+    return (
+      <p className={`text-sm font-mono leading-4 ${className}`}>{children}</p>
+    );
+  }
+  return (
+    <p className={`text-sm font-normal leading-5 ${className}`}>{children}</p>
+  );
 };
 
 export default Typography;
---
+++ src/pages/error/UnknownErrorPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Typography from 'components/Typography';
+import PathName from 'constants/PathName';
+
+interface UnknownErrorPageProps {
+  error: Error;
+}
+
+const UnknownErrorPage: React.FC<UnknownErrorPageProps> = ({ error }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex justify-between items-center flex-col pt-12 pl-12 pr-12 pb-12 h-screen gap-6">
+      <div className="flex gap-2 flex-col w-full">
+        <Typography type="title">{t('unknown-error-page-title')}</Typography>
+        <Typography type="body">
+          {t('unknown-error-page-description')}
+        </Typography>
+      </div>
+      <div className="flex flex-col w-full h-full gap-2 overflow-auto">
+        <Typography type="code">{`${t('unknown-error-occur-message')} : ${
+          error.message
+        }`}</Typography>
+        {error.stack?.split('\n').map((errorStack) => (
+          <Typography className="break-all" type="code" key={errorStack}>
+            {errorStack}
+          </Typography>
+        ))}
+      </div>
+      <a href={PathName.MAP_PAGE}>
+        <button
+          type="button"
+          className="px-6 py-2 rounded-md bg-primary hover:pg-primary-hover text-white w-fit"
+        >
+          {t('back-to-home-button-text')}
+        </button>
+      </a>
+    </div>
+  );
+};
+
+export default UnknownErrorPage;
