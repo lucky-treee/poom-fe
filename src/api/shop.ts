20eb212c5acdcbc6f528a438faba3bad817baf85
Minsup Lee <m1nsuppp@gmail.com>
2023-02-21 22:46:53 +0900

    refactor: 불필요한 인터페이스 제거

--- src/api/shop.ts
+++ src/api/shop.ts
@@ -1,10 +1,4 @@
 import client from 'api';
-import { LatitudeType, LongitudeType } from 'models/Location';
-
-export interface Coordinate {
-  latitude: LatitudeType;
-  longitude: LongitudeType;
-}
 
 // 아직 정보 없음
 interface Shop {
@@ -18,12 +12,13 @@ interface ShopResponse {
   result: Shop[];
 }
 
-export const getShopByCoordinate = (coordinate: Coordinate) => {
-  const { latitude, longitude } = coordinate;
-  const { maxLat, minLat } = latitude;
-  const { maxLng, minLng } = longitude;
-
-  return client().get<ShopResponse>(
+export const fetchShopList = (
+  maxLat: number,
+  minLat: number,
+  maxLng: number,
+  minLng: number
+) => {
+  return client.get<ShopResponse>(
     `/shop/get/${maxLat}/${minLat}/${maxLng}/${minLng}`
   );
 };
