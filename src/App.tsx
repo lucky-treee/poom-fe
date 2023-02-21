e7aa1a5bd093ced0ae816ecf971981a322b0ba5b
Minsup Lee <m1nsuppp@gmail.com>
2023-02-21 19:43:33 +0900

    feat: config `react-query`

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,31 @@ import SignUpPage from 'pages/SignUpPage';
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryCache = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={PathName.MAP_PAGE} element={<MapPage />} />
-          <Route path={PathName.LOGIN_PAGE} element={<LoginPage />} />
-          <Route path={PathName.SIGNUP_PAGE} element={<SignUpPage />} />
-          <Route path={PathName.PROFILE_PAGE} element={<ProfilePage />} />
-          <Route path={PathName.SHOP_PAGE} element={<ShopPage />} />
-        </Routes>
-      </BrowserRouter>
-    </React.Suspense>
+    <QueryClientProvider client={queryCache}>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={PathName.MAP_PAGE} element={<MapPage />} />
+            <Route path={PathName.LOGIN_PAGE} element={<LoginPage />} />
+            <Route path={PathName.SIGNUP_PAGE} element={<SignUpPage />} />
+            <Route path={PathName.PROFILE_PAGE} element={<ProfilePage />} />
+            <Route path={PathName.SHOP_PAGE} element={<ShopPage />} />
+          </Routes>
+        </BrowserRouter>
+      </React.Suspense>
+    </QueryClientProvider>
   );
 }
 
