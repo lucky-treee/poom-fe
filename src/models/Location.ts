e04b64ad158fc4b003db97fbc1ef8156632ac6a1
Minsup Lee <m1nsuppp@gmail.com>
2023-02-21 19:42:56 +0900

    feat: define coordinate type

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -1,3 +1,13 @@
+export type LatitudeType = {
+  maxLat: number | undefined;
+  minLat: number | undefined;
+};
+
+export type LongitudeType = {
+  maxLng: number | undefined;
+  minLng: number | undefined;
+};
+
 export type LocationType = {
   lat: number;
   lng: number;
