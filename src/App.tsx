33b2e0349b0ac2414b2054c1b403798eb20227dc
Minsup Lee <m1nsuppp@gmail.com>
2023-02-21 23:13:08 +0900

    refactor: use `@tanstack/react-query`

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import SignUpPage from 'pages/SignUpPage';
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryCache = new QueryClient({
   defaultOptions: {
