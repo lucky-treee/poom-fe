4ef853498c56c206276f688801eeda3789e765ab
Minsup Lee <m1nsuppp@gmail.com>
2023-02-22 14:11:30 +0900

    refactor: 필요없는 interface 제거

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -1,13 +1,3 @@
-export type LatitudeType = {
-  maxLat: number | undefined;
-  minLat: number | undefined;
-};
-
-export type LongitudeType = {
-  maxLng: number | undefined;
-  minLng: number | undefined;
-};
-
 export type LocationType = {
   lat: number;
   lng: number;
