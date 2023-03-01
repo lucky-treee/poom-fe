3b05efd059dcbcaf47933d12e4344e7b3a9300f5
sshrik <chj878194@naver.com>
2023-03-02 00:39:15 +0900

    feat: add LoginAlertProfile

---
+++ src/models/auth/LoginAlertProfile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PathName from 'constants/PathName';
+import Typography from 'components/Typography';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+interface LoginAlertProfileProps {
+  className?: string;
+}
+
+const LoginAlertProfile: React.FC<LoginAlertProfileProps> = (props) => {
+  const { className } = props;
+
+  const { t } = useTranslation();
+
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <div className="flex flex-row gap-2 items-center">
+        <Typography type="subtitle">{t('need-login-message')}</Typography>
+        <Link
+          className="text-sm font-normal leading-5 text-sky-500 underline"
+          to={PathName.LOGIN_PAGE}
+        >
+          {t('login-link-text')}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LoginAlertProfile;
