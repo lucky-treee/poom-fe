c762bab3a5b4b156ba7e2a3abca352759277c902
sshrik <chj878194@naver.com>
2023-03-03 00:32:27 +0900

    fix: fix auth service type error

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -2,11 +2,11 @@ import { LogInResponse, ProfileResponse } from 'models/auth/response';
 import { AuthService } from 'service';
 
 export const login = (code: string) => {
-  return AuthService.get<unknown, LogInResponse>('/auth/login', {
+  return AuthService.get<LogInResponse>('/auth/login', {
     params: { code },
   });
 };
 
 export const fetchProfile = () => {
-  return AuthService.get<unknown, ProfileResponse>('/auth/profile');
+  return AuthService.get<ProfileResponse>('/auth/profile');
 };
