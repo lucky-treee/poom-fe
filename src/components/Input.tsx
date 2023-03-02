2a6f610cc2c7bb0dbf57d728daff6e26f8bd24a0
sshrik <chj878194@naver.com>
2023-03-02 23:47:26 +0900

    refactor: refactor `InputBox` to `Input` component

---
+++ src/components/Input.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { RegisterOptions, useFormContext } from 'react-hook-form';
+
+type InputProps<T> = {
+  name: string;
+  className?: string;
+  accept?: string;
+  placeholder?: T;
+  defaultValue?: T;
+  type?: React.HTMLInputTypeAttribute;
+} & RegisterOptions;
+
+const style = {
+  normal: 'focus:border-sky-500',
+  error: 'border-red-500',
+};
+
+function Input<T>(props: InputProps<T>) {
+  const { name, placeholder, type, className, accept, ...registerOptions } =
+    props;
+
+  const { register, formState, getFieldState } = useFormContext();
+
+  const { error } = getFieldState(name, formState);
+
+  return (
+    <div className="flex flex-col w-full">
+      <input
+        className={`border outline-0 w-full px-4 py-3 border-border-gray rounded-md  ${
+          error ? style.error : style.normal
+        } ${className}`}
+        accept={accept}
+        type={type}
+        placeholder={typeof placeholder === 'string' ? placeholder : undefined}
+        {...register(name, registerOptions)}
+      />
+      {error && (
+        <p className="text-xs w-full px-4 pt-2 text-red-500">{error.message}</p>
+      )}
+    </div>
+  );
+}
+
+export default Input;
---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StoryFn } from '@storybook/react';
+import Input from 'components/Input';
+import 'index.css';
+
+export default {
+  title: 'Input Box',
+  component: Input,
+};
+
+export const Default: StoryFn<typeof Input> = () => (
+  <Input name="example" placeholder="Placeholder" />
+);
