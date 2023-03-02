9feedaad41fc7727814f812f1b252ba0ae952449
sshrik <chj878194@naver.com>
2023-03-03 00:07:36 +0900

    feat: add `NicknameInput`

---
+++ src/components/auth/NicknameInput.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Input from 'components/Input';
+import { useTranslation } from 'react-i18next';
+
+const NicknameInput: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Input
+      required={{
+        value: true,
+        message: t('nickname-required-error-msg'),
+      }}
+      maxLength={{
+        value: 10,
+        message: t('nickname-length-error-msg'),
+      }}
+      minLength={{
+        value: 2,
+        message: t('nickname-length-error-msg'),
+      }}
+      pattern={{
+        value: /^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]+$/,
+        message: t('nickname-pattern-error-msg'),
+      }}
+      name="nickname"
+      placeholder={t('nickname')}
+    />
+  );
+};
+
+export default NicknameInput;
