ca83a18224a47d3ebda1957a3916f7bff181165b
sshrik <chj878194@naver.com>
2023-03-25 16:39:43 +0900

    fix: fix member api url

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -3,16 +3,21 @@ import { LogInResponse, ProfileResponse } from 'models/auth/response';
 import { SignUpForm } from 'models/auth/SignUpForm';
 import { AuthService } from 'service';
 
-export const login = (code: string) => {
-  return AuthService.get<LogInResponse>('/auth/login', {
-    params: { code },
+export const login = () => {
+  return AuthService.get<LogInResponse>('/v1/auth/login');
+};
+
+export const kakaoLogin = (code: string, redirect: string) => {
+  return AuthService.post('/v1/auth/login/kakao', {
+    code,
+    redirect_uri: redirect,
   });
 };
 
 export const fetchProfile = () => {
-  return AuthService.get<ProfileResponse>('/auth/profile');
+  return AuthService.get<ProfileResponse>('/v1/members/profile');
 };
 
 export const signUp = (formValue: SignUpForm) => {
-  return AuthService.post<SignUpRequest>('/auth/signup', { ...formValue });
+  return AuthService.post<SignUpRequest>('/v1/auth/signup', { ...formValue });
 };
