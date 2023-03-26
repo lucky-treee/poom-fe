6847744667d1abafaa3185208a8ffe89e2faa192
sshrik <chj878194@naver.com>
2023-03-26 15:26:44 +0900

    feat: add local-poom host config and mkcert service

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
+import mkcert from 'vite-plugin-mkcert';
 import svgr from 'vite-plugin-svgr';
 import tsconfigpaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigpaths(), VitePluginHtmlEnv(), svgr()],
+  plugins: [react(), tsconfigpaths(), VitePluginHtmlEnv(), svgr(), mkcert()],
   server: {
+    host: 'local-poom.c0dewave.com',
+    https: true,
     proxy: {
       '^/api/shop/.*': {
         target: 'https://poom-shop.c0dewave.com',
