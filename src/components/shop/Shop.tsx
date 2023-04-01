54427a0d640c61d4ffe24672625b18ae750cc6fd
Minsup Lee <m1nsuppp@gmail.com>
2023-04-02 00:04:56 +0900

    refactor: move handleGoBack to `<Shop />`

--- src/components/shop/Shop.tsx
+++ src/components/shop/Shop.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ReactComponent as HeartIcon } from 'assets/components/Heart.svg';
+import { ReactComponent as BackArrowIcon } from 'assets/components/navigate/Back.svg';
 import HashtagChip from 'components/HashtagChip';
 import Typography from 'components/Typography';
 import { Shop as ShopType } from 'models/shop/Shop';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
 
 type ShopProps = {
   shop: ShopType;
@@ -11,27 +13,33 @@ type ShopProps = {
 };
 
 const Shop: React.FC<ShopProps> = ({ shop, review }) => {
+  const navigate = useNavigate();
+  const handleGoBack = () => navigate(-1);
   const { t } = useTranslation();
 
   return (
-    <div className="mx-[29px] mb-[70px]">
+    <div className="mb-6">
       <div>
         <div className="flex justify-between items-center">
-          <Typography type="title" className="text-text">
-            {shop.shopName}
-          </Typography>
+          <div className="flex">
+            <button type="button" className="mr-5" onClick={handleGoBack}>
+              <BackArrowIcon />
+            </button>
+            <Typography type="title" className="text-text">
+              {shop.shopName}
+            </Typography>
+          </div>
           <HeartIcon />
         </div>
-        <Typography type="caption" className="text-disabled">
-          {shop.category}
-        </Typography>
       </div>
-      <div className="w-[95%] flex justify-between items-center text-text mt-2">
+      <div className="w-[95%] text-text mt-2">
+        <Typography type="caption" className="text-disabled">
+          {shop.flagshipProduct}
+        </Typography>
         <Typography type="body" className="inline">
           {shop.shopAddress}
         </Typography>
-        <div className="h-3 divider border-l border-border-gray" />
-        <Typography type="body" className="inline">
+        <Typography type="body">
           {t('review-count-message', {
             reviewCount: review.toLocaleString(),
           })}
