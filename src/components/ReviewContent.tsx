40b092992ca4005b78749eeb8af850c8ad0a43f5
Minsup Lee <m1nsuppp@gmail.com>
2023-04-02 00:00:28 +0900

    feat: conditional rendering updated date pathname

--- src/components/ReviewContent.tsx
+++ src/components/ReviewContent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LocalDate from 'components/LocalDate';
 import ReviewImageList from 'components/ReviewImageList';
 import Typography from 'components/Typography';
+import { useLocation } from 'react-router';
 
 type ReviewContentProps = {
   imgSrcList: string[];
@@ -10,13 +11,15 @@ type ReviewContentProps = {
 };
 
 const ReviewContent: React.FC<ReviewContentProps> = (props) => {
+  const location = useLocation();
+  const { pathname } = location;
   const { imgSrcList, content, updatedAt } = props;
 
   return (
     <div className="flex flex-col gap-2 w-full">
       <ReviewImageList imgSrcList={imgSrcList} />
       <Typography type="body">{content}</Typography>
-      <LocalDate date={updatedAt} />
+      {!pathname.includes('shop') && <LocalDate date={updatedAt} />}
     </div>
   );
 };
