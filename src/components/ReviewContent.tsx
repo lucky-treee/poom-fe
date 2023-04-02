496edc3ab767fbfa97e831d51fe5566ba586ad75
Minsup Lee <m1nsuppp@gmail.com>
2023-04-02 23:27:01 +0900

    refactor: rollback

--- src/components/ReviewContent.tsx
+++ src/components/ReviewContent.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import LocalDate from 'components/LocalDate';
 import ReviewImageList from 'components/ReviewImageList';
 import Typography from 'components/Typography';
-import { useLocation } from 'react-router';
 
 type ReviewContentProps = {
   imgSrcList: string[];
@@ -11,15 +10,13 @@ type ReviewContentProps = {
 };
 
 const ReviewContent: React.FC<ReviewContentProps> = (props) => {
-  const location = useLocation();
-  const { pathname } = location;
   const { imgSrcList, content, updatedAt } = props;
 
   return (
     <div className="flex flex-col gap-2 w-full">
       <ReviewImageList imgSrcList={imgSrcList} />
       <Typography type="body">{content}</Typography>
-      {!pathname.includes('shop') && <LocalDate date={updatedAt} />}
+      <LocalDate date={updatedAt} />
     </div>
   );
 };
