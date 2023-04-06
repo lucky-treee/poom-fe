8344b47b91eca8cdac7c89716e361e84d7f530d5
sshrik <chj878194@naver.com>
2023-04-06 22:41:46 +0900

    storybook: add storybook `Select` and fix `Input` story

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -2,12 +2,31 @@ import React from 'react';
 import { StoryFn } from '@storybook/react';
 import Input from 'components/base/Input';
 import 'index.css';
+import { FormProvider, useForm } from 'react-hook-form';
 
 export default {
   title: 'Input',
   component: Input,
 };
 
-export const Default: StoryFn<typeof Input> = () => (
-  <Input name="example" placeholder="Placeholder" />
-);
+export const Default: StoryFn<typeof Input> = () => {
+  const methods = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      example: 'values',
+    },
+  });
+
+  return (
+    <FormProvider {...methods}>
+      <Input
+        name="example"
+        placeholder="Placeholder"
+        required={{
+          value: true,
+          message: '필수 입력 항목 입니다.',
+        }}
+      />
+    </FormProvider>
+  );
+};
