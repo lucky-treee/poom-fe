e4677bca87f99aeca4f56bbb294225092d70274e
sshrik <chj878194@naver.com>
2023-04-07 17:32:05 +0900

    feat: create ToastContext  and Provider

---
+++ src/components/ToastProvider.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode, useReducer } from 'react';
+import Toast from 'components/Toast';
+import {
+  ToastContext,
+  ToastDispatchContext,
+  ToastReducer,
+} from 'models/ToastContext';
+
+type ToastContainerProps = {
+  children?: ReactNode;
+};
+
+const ToastProvider: React.FC<ToastContainerProps> = (props) => {
+  const { children } = props;
+
+  const [toastList, dispatch] = useReducer(ToastReducer, []);
+
+  return (
+    <ToastDispatchContext.Provider value={dispatch}>
+      <ToastContext.Provider value={toastList}>
+        {children}
+        <div className="absolute top-8 right-4 flex flex-col gap-4 z-10">
+          {toastList.map((toast) => (
+            <Toast key={toast.id} {...toast} />
+          ))}
+        </div>
+      </ToastContext.Provider>
+    </ToastDispatchContext.Provider>
+  );
+};
+
+export default ToastProvider;
