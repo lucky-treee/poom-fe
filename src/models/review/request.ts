43282c0d21a911363396285c9baa462066b62df6
Minsup Lee <m1nsuppp@gmail.com>
2023-04-12 01:00:49 +0900

    refactor: remove `memberId`

--- src/models/review/request.ts
+++ src/models/review/request.ts
@@ -2,6 +2,5 @@ import { ReviewRegisterForm } from 'models/review/ReviewRegisterForm';
 
 export type RegisterReviewRequest = {
   images: File[];
-  memberId: number;
   shopId: number;
 } & ReviewRegisterForm;
